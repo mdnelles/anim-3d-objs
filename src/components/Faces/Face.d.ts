cce917394b77fb43fa4d8c0c61ea27d7ca14ec58
mdnelles <mxnelles@gmail.com>
2022-06-04 09:27:48 -0500

    add interface file

---
+++ src/components/Faces/Face.d.ts
@@ -0,0 +1,18 @@
+export interface FaceBottomProps {
+   bfv?: string | boolean;
+   bgc?: string | boolean;
+   border?: string | boolean;
+   children?: string | any;
+   fgStyles?: object | any;
+   depth?: number | any;
+   height?: number | string;
+   id?: string | number;
+   left?: number;
+   margin?: number | string;
+   opac?: number | string | boolean | undefined;
+   padding?: number | string;
+   position?: number;
+   top?: number;
+   tranz?: number | string;
+   width?: number | string;
+}
