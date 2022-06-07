e146bd434cac49c58c1f356866dc98bad30bbaeb
mdnelles <mxnelles@gmail.com>
2022-06-07 05:36:27 -0500

    add ts file for style props

---
+++ src/components/styles/Anim.d.ts
@@ -0,0 +1,8 @@
+export interface AnimStylesProps {
+   duration?: number | string;
+   iterationCount?: number | string;
+   animName?: string;
+   fillMode?: string;
+   children?: any;
+   borderColor?: string;
+}
