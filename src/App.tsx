465d1c41113bb8a9d248b9ff4c4a3769a09e4384
mdnelles <mxnelles@gmail.com>
2022-06-10 11:21:00 -0500

    all face colors and styles installed

--- src/App.tsx
+++ src/App.tsx
@@ -13,15 +13,15 @@ function App() {
       // global styles
       border: "1px solid #00f",
       bgc: "#009",
-      opac: 1,
+      opac: 0.9,
       bfv: "visible",
       fontFamily: "Cochin",
    };
    const animSpecs: object = {
       anim1: "Y360",
-      anim2: "",
+      anim2: "X360",
       anim1duration: 5,
-      anim2duration: 3,
+      anim2duration: 23,
       anim1count: "infinite",
       anim1low: "-120",
       anim2hi: "120",
@@ -30,19 +30,7 @@ function App() {
       anim1direction: "normal",
       anim2direction: "normal",
    };
-   const animSpecs2: object = {
-      anim1: "Y360",
-      anim2: "",
-      anim1duration: 5,
-      anim2duration: 3,
-      anim1count: "infinite",
-      anim1low: "-120",
-      anim2hi: "120",
-      anim2low: "-81",
-      anim2count: "infinite",
-      anim1direction: "reverse",
-      anim2direction: "reverse",
-   };
+
    const indivStyles: object = {
       // // face individual styles (over rides global)
       bottom: {
@@ -55,21 +43,30 @@ function App() {
          bfv: "visible",
          fontFamily: "Arial, Sans",
       },
+      left: {
+         bgc: "yellow",
+      },
+      top: {
+         bgc: "pink",
+      },
+      right: {
+         bgc: "purple",
+      },
       back: {
          border: "1px solid #f00",
-         bgc: "#f00",
+         bgc: "#0f0",
          bfv: "visible",
          fontFamily: "Arial, Sans",
       },
    };
 
    return (
-      <div style={{ padding: 100 }}>
+      <div style={{ padding: 150 }}>
          <Cuboid
             width={260}
             depth={260}
             height={92}
-            perspectiveOrigin='50% 130%'
+            perspectiveOrigin='50% 50%'
             zIndex={10}
             animSpecs={animSpecs}
             indivStyles={indivStyles}
@@ -78,34 +75,7 @@ function App() {
          >
             {}
          </Cuboid>
-         <div style={{ padding: 10 }} />
-         <Cuboid
-            width={260}
-            depth={260}
-            height={52}
-            perspectiveOrigin='50% 50%'
-            zIndex={1}
-            animSpecs={animSpecs2}
-            indivStyles={indivStyles}
-            faces={faceprops}
-            globalStyles={globalStyles}
-         >
-            {}
-         </Cuboid>
-         <div style={{ padding: 10 }} />
-         <Cuboid
-            width={260}
-            depth={260}
-            height={92}
-            perspectiveOrigin='50% -50%'
-            zIndex={1}
-            animSpecs={animSpecs}
-            indivStyles={indivStyles}
-            faces={faceprops}
-            globalStyles={globalStyles}
-         >
-            {}
-         </Cuboid>
+         <div style={{ padding: 5 }} />
       </div>
    );
 }
