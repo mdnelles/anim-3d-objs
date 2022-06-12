1f70defaa4a3d6b3b42b022971be6f75721be223
mdnelles <mxnelles@gmail.com>
2022-06-12 12:12:17 -0500

    resolve eval() issue for typscript

--- src/components/styles/AnimWrap.tsx
+++ src/components/styles/AnimWrap.tsx
@@ -35,6 +35,26 @@ export const AnimWrap = (props: AnimStylesProps) => {
       fwdy2736,
       noAnim,
    } = allAnims({ animLow, animHi });
+   let theAnim: any = "noAnim";
+   // need to iterate through all animation posibilities and not use eval() to satisfy TS
+   if (animName === "X360") theAnim = X360;
+   else if (animName === "Y360") theAnim = Y360;
+   else if (animName === "fadeInkf") theAnim = fadeInkf;
+   else if (animName === "wobX") theAnim = wobX;
+   else if (animName === "wobY") theAnim = wobY;
+   else if (animName === "fwdx018") theAnim = fwdx018;
+   else if (animName === "fwdx1836") theAnim = fwdx1836;
+   else if (animName === "fwdx09") theAnim = fwdx09;
+   else if (animName === "fwdx918") theAnim = fwdx918;
+   else if (animName === "fwdx1827") theAnim = fwdx1827;
+   else if (animName === "fwdx2736") theAnim = fwdx2736;
+   else if (animName === "fwdy018") theAnim = fwdy018;
+   else if (animName === "fwdy1836") theAnim = fwdy1836;
+   else if (animName === "fwdy09") theAnim = fwdy09;
+   else if (animName === "fwdy918") theAnim = fwdy918;
+   else if (animName === "fwdy1827") theAnim = fwdy1827;
+   else if (animName === "fwdy2736") theAnim = fwdy2736;
+   else theAnim = noAnim;
 
    const AnimWrapDiv: any = styled.div`
       width: 100%;
@@ -46,8 +66,8 @@ export const AnimWrap = (props: AnimStylesProps) => {
       animation-duration: ${duration}s;
       -webkit-animation-iteration-count: ${iterationCount};
       animation-iteration-count: ${iterationCount};
-      -webkit-animation-name: ${eval(animName)};
-      animation-name: ${eval(animName)};
+      -webkit-animation-name: ${theAnim};
+      animation-name: ${theAnim};
       -webkit-animation-fill-mode: ${fillMode};
       animation-fill-mode: ${fillMode};
       animation-direction: ${animDirection};
